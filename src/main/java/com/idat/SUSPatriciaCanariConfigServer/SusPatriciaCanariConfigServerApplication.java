356413371e7260fa436cf88f15013dfdbae8cacf
Pcanari <patriciacanarihuaman@gmail.com>
2022-09-02 20:21:21 -0500

    config

---
+++ src/main/java/com/idat/SUSPatriciaCanariConfigServer/SusPatriciaCanariConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.idat.SUSPatriciaCanariConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusPatriciaCanariConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SusPatriciaCanariConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idat/SUSPatriciaCanariConfigServer/SusPatriciaCanariConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.SUSPatriciaCanariConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SusPatriciaCanariConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
